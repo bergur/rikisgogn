faa3dbb1f47b8bd0c74d2581ef1bfd43e0b8e262
Bergur Hallgrímsson <bergur@bergur.is>
2020-09-17 10:53:51 +0000

    securing cookie

--- routes/login.js
+++ routes/login.js
@@ -4,10 +4,18 @@ function makeLoginRouter (login) {
   const router = express.Router()
 
   router.post('/', (req, res, next) => {
+    const cookieOptions = {
+      domain: '.rikisgogn.is',
+      secure: process.env.NODE_Env === 'production',
+      httpOnly: true,
+      sameSite: true,
+      path: '/'
+    }
+
     login(req.body.email, req.body.password)
       .then(user => {
         res
-          .cookie('RIKISGOGN_TOKEN', user.token)
+          .cookie('RIKISGOGN_TOKEN', user.token, cookieOptions)
           .send(user)
       })
       .catch(next)
